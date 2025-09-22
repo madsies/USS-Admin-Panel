640a07e61c508a3333681d46ec4fb64d6ba6f5fb
madsies <madsiesss@gmail.com>
2025-09-22 04:42:39 +0100

    Improved discord bot

--- src/main/Python/bot.py
+++ src/main/Python/bot.py
@@ -4,14 +4,21 @@ from discord.ext import commands
 from os import listdir
 from datetime import datetime
 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
 class MyBot(commands.Bot):
     async def on_ready(self):
         print(f"[{datetime.now().strftime('%H:%M:%S')}] Logged on as {self.user}")
-        return await super().on_ready()
     
     async def load_extension(self, name, *, package = None):
         return await super().load_extension(name, package=package)
@@ -27,6 +34,58 @@ class MyBot(commands.Bot):
         await self.tree.sync() # Updates Slash Commands
         return await super().setup_hook()    
     
+class SheetsManagement():
+    def __init__(self):
+        creds = None
+
+        self.ADMIN_SHEET = os.environ.get("ADMIN_SHEET");
+
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+                CREDS_DIR = os.path.join(BASE_DIR, "credentials.json")
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    CREDS_DIR, SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
+
+        try:
+            self.service = build("sheets", "v4", credentials=creds)
+            self.sheet = self.service.spreadsheets()
+
+        except HttpError as err:
+            print(err)
+
+    def write_data(self, data, query):
+        body = {"values": data}
+        result = (
+            self.service.spreadsheets()
+            .values()
+            .update(
+                spreadsheetId=self.ADMIN_SHEET,
+                range=query,
+                valueInputOption="USER_ENTERED",
+                body=body,
+            )
+            .execute())
+    
+
+    def read_data(self, query):
+        result = (
+        self.sheet.values()
+        .get(spreadsheetId=self.ADMIN_SHEET, range=query)
+        .execute())
+
+        return result.get("values", [])
+
 
 def main():
     INTENTS = discord.Intents.default()
@@ -36,6 +95,8 @@ def main():
     client = MyBot(command_prefix='!', intents=INTENTS)
     
     client.run(os.environ.get('DISCORD_BOT_TOKEN'))
+
+   
     
 
     
--- src/main/Python/cogs/checkIn.py
+++ src/main/Python/cogs/checkIn.py
@@ -1,13 +1,25 @@
 import discord
 from discord.ext import commands
 
+import sys
+from pathlib import Path
+
+parent_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(parent_dir))
+
+from bot import SheetsManagement
+
 class CheckInCommands(commands.Cog):
-    def __init__(self):
-        return
+    def __init__(self, bot):
+        self.manager = SheetsManagement()
     
-    @commands.hybrid_command(name="CheckIn")
+    @commands.hybrid_command(name="checkin")
     async def check_in(self, ctx):
         sender : discord.Member = ctx.author
+
+        data = self.manager.read_data("DataSheet!A1")
+
+        await ctx.reply(data)
 
         # Check database if username is in list
         # if username in list
@@ -19,8 +31,8 @@ class CheckInCommands(commands.Cog):
         #    say you arent a captain
 
 
-    @commands.hybrid_command(name="CheckOut")
-    async def check_out(self, ctx)
+    @commands.hybrid_command(name="checkout")
+    async def check_out(self, ctx):
         sender : discord.Member = ctx.author
 
         # Check database if username is in list
@@ -31,3 +43,7 @@ class CheckInCommands(commands.Cog):
         #       check team out
         #else
         #    say you arent a captain
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(CheckInCommands(bot))
